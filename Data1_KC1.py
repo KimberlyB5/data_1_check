fe79dd72daa316fb02b9d77e0c7776537524e55c
Kimberly Burba <kam52599@gmail.com>
2022-05-20 20:04:29 -0400

    knowledge check 1

---
+++ Data1_KC1.py
@@ -0,0 +1,36 @@
+# Hello World
+
+print('\nHello World!')
+
+# List - Hypoallergenic Cat Breeds
+
+cats = ['Balinese', 'Cornish Rex','Devon Rex', 'Javanese', 'Oriental', 'Burmese', ]
+print(cats[5])
+
+# Dictionary - Some of the top 10 cat breeds for people with allergies
+
+allergy = {}
+
+allergy[1] = 'Balinese'
+allergy[2] = 'Cornish Rex'
+allergy[3] = 'Devon Rex'
+allergy[8] = 'Javanese'
+allergy[7] = 'Oriental'
+allergy[10] = 'Burmese'
+
+
+print(allergy[10], 'is number 10 on the Best Cat Breeds for Allergy Suffers')
+
+#Tuple - characteristics of Burmese cats
+
+Burmese = ('Sturdy', 'Stocky', 'Curious', 'Active', 'Short hair', 'Glossy Coat', 'Gold eyes', '8 to 10 inches tall', '8 to 12 pounds')
+
+print(Burmese[:4])
+print(Burmese[4:])
+print(Burmese[6])
+
+
+
+
+
+
